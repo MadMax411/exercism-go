83e731d1e7dc03522fecbc715539fbd1c5ff2a55
Kerri Miller <kerrizor@github.com>
2016-09-07 13:13:23 -0700

    Just return from the boolean

--- leap/leap.go
+++ leap/leap.go
@@ -3,9 +3,5 @@ package leap
 const testVersion = 2
 
 func IsLeapYear(year int) bool {
-	if year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0) {
-		return true
-	}
-
-	return false
+	return year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0)
 }
