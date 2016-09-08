0d7ff1c09c19a2d4db5fc4c8d8afe68d86d27c1f
Kerri Miller <kerrizor@github.com>
2016-09-08 14:39:51 -0700

    First solution for exercize 7: bob

--- bob/bob.go
+++ bob/bob.go
@@ -1,3 +1,24 @@
-package bob // package name must match the package name in bob_test.go
+package bob
 
-const testVersion = 2 // same as targetTestVersion
+import "strings"
+
+const testVersion = 2
+
+func Hey(greeting string) string {
+  var response string
+
+  greeting = strings.TrimSpace(greeting)
+
+  switch {
+  case greeting == "":
+    response = "Fine. Be that way!"
+  case greeting == strings.ToUpper(greeting) && greeting != strings.ToLower(greeting):
+    response = "Whoa, chill out!"
+  case strings.HasSuffix(greeting, "?"):
+    response = "Sure."
+  default:
+    response = "Whatever."
+  }
+
+  return response
+}
